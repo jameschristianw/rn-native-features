ce4eb11687202a3a43908b40ba30521c5329f6bd
James Christian Wira <jamesch888@gmail.com>
2022-03-26 18:37:59 +0700

    Fix: Move API key to another location

--- util/location.js
+++ util/location.js
@@ -1,4 +1,6 @@
-const GOOGLE_API_KEY = "AIzaSyCds1SE0pKInApjNGhFXxz5kM450YT5Yxk";
+import { GOOGLE_API_KEY } from "../secrets/key";
+
+const GOOGLE_API_KEY = GOOGLE_API_KEY;
 
 export const getMapPreview = (latitude, longitude) => {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
