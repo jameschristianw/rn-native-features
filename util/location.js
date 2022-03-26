c1eceab6b51056598760da0fb7442082c03cfa0d
James Christian Wira <jamesch888@gmail.com>
2022-03-26 19:03:11 +0700

    Move API Key

--- util/location.js
+++ util/location.js
@@ -1,7 +1,5 @@
 import { GOOGLE_API_KEY } from "../secrets/key";
 
-const GOOGLE_API_KEY = GOOGLE_API_KEY;
-
 export const getMapPreview = (latitude, longitude) => {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
